7f863c2825b90a18eaa3700f8adf702f939fd164
gabrielAOR <80289745+gabrielAOR@users.noreply.github.com>
2024-09-17 21:30:43 -0300

    Update spreadsheet.py

--- spreadsheet.py
+++ spreadsheet.py
@@ -20,72 +20,71 @@ CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 TOKEN = os.getenv('REFRESH_TOKEN')
 
 def get_balance():
-  coins_values = get_account_balances()
-  values = [[]]
-  today = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
-  values[0].append(today)
-  
-  for coin, value in coins_values:
-      values[0].append(round(value,2))
-  print(values)
-  return values
+    coins_values = get_account_balances()
+    values = [[]]
+    today = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+    values[0].append(today)
+
+    for coin, value in coins_values:
+        values[0].append(round(value, 2))
+    print(values)
+    return values
 
 @handle_http_error
-def nearest_empty_cell(creds,column='A'):
-  range = f'{column}1:{column}1000'
-  service = build("sheets", "v4", credentials=creds)
+def nearest_empty_cell(creds, column='A'):
+    range = f'{column}1:{column}1000'
+    service = build("sheets", "v4", credentials=creds)
 
-  result = (
-      service.spreadsheets()
-      .values()
-      .get(spreadsheetId=SPREADSHEET_ID, range=range)
-      .execute()
-  )
-  index = len(result.get("values", [])) + 1
-  return f'{column}{index}'
+    result = (
+        service.spreadsheets()
+        .values()
+        .get(spreadsheetId=SPREADSHEET_ID, range=range)
+        .execute()
+    )
+    index = len(result.get("values", [])) + 1
+    return f'{column}{index}'
 
 def get_right_creds():
-  creds = None
+    creds = None
 
-  if CLIENT_ID and CLIENT_SECRET and REFRESH_TOKEN:
-    # Build the credentials using the refresh token
-    creds_data = {
-        "token": None,  # Initially no token
-        "refresh_token": REFRESH_TOKEN,
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "client_id": CLIENT_ID,
-        "client_secret": CLIENT_SECRET,
-        "scopes": SCOPES
-    }
-    creds = Credentials.from_authorized_user_info(creds_data, SCOPES)
+    if CLIENT_ID and CLIENT_SECRET and TOKEN:
+        # Build the credentials using the refresh token
+        creds_data = {
+            "token": None,  # Initially no token
+            "refresh_token": TOKEN,
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
+            "scopes": SCOPES
+        }
+        creds = Credentials.from_authorized_user_info(creds_data, SCOPES)
 
-    # Refresh the credentials if they are expired
-    if creds and creds.expired and creds.refresh_token:
-      try:
-        creds.refresh(Request())
-      except RefreshError:
-        raise Exception("Failed to refresh access token. Check if the refresh token is valid.")
+        # Refresh the credentials if they are expired
+        if creds and creds.expired and creds.refresh_token:
+            try:
+                creds.refresh(Request())
+            except Exception as e:
+                raise Exception(f"Failed to refresh access token. {str(e)}")
     else:
-      raise Exception("Missing CLIENT_ID, CLIENT_SECRET, or REFRESH_TOKEN in environment variables.")
+        raise Exception("Missing CLIENT_ID, CLIENT_SECRET, or REFRESH_TOKEN in environment variables.")
 
-  return creds
-  
+    return creds
+
 @handle_http_error
 def update_transactions(creds):
-  range = nearest_empty_cell(creds)
-  print('started to update')
-  service = build("sheets", "v4", credentials=creds)
-  service.spreadsheets().values().update(
-      spreadsheetId=SPREADSHEET_ID,
-      range=range,
-      valueInputOption="USER_ENTERED",
-      body={"values": get_balance()}
-  ).execute()
+    range = nearest_empty_cell(creds)
+    print('started to update')
+    service = build("sheets", "v4", credentials=creds)
+    service.spreadsheets().values().update(
+        spreadsheetId=SPREADSHEET_ID,
+        range=range,
+        valueInputOption="USER_ENTERED",
+        body={"values": get_balance()}
+    ).execute()
 
 def main():
-  credentials = get_right_creds()
-  update_transactions(CREDENTIALS)
-
+    credentials = get_right_creds()  # Get the correct credentials object
+    update_transactions(credentials)  # Pass the credentials object to the function
 
 if __name__ == "__main__":
-  main()
+    main()
